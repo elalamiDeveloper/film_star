72fc9fda165a5ba1e3eeed0097f6896d834bceb7
El Alami Idriss <elalami.developer.1991@gmail.com>
2023-05-23 15:24:46 +0200

    Add tmbd api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,18 @@
-const App = () => {};
+import { useEffect } from 'react';
+
+import fetchDataFromApi from './utils/api';
+
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDataFromApi('/movie/popular');
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>App</div>;
+};
 
 export default App;
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
-const TMDB_TOKEN = import.meta.env.APP_TMDB_TOKEN;
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-  Authorization: `bearer ${TMDB_TOKEN}`,
+  accept: 'application/json',
+  Authorization: `Bearer ${TMDB_TOKEN}`,
 };
 
 const fetchDataFromApi = async (url, params) => {
@@ -21,4 +22,4 @@ const fetchDataFromApi = async (url, params) => {
   }
 };
 
-export { fetchDataFromApi };
+export default fetchDataFromApi;
